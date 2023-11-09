63ea6830252d157876a19ba3efd30b7ebf979721
talen1 <90649409+talen1@users.noreply.github.com>
2023-11-09 15:47:28 -0800

    ExpressionManager.cpp

---
+++ ExpressionManager.cpp
@@ -0,0 +1,17 @@
+#include "ExpressionManager.h"
+
+bool ExpressionManager::isOperator(char c) const {
+    // Implementation to check if the character is an operator
+}
+
+int ExpressionManager::precedence(char op) const {
+    // Implementation to return the precedence of the operator
+}
+
+bool ExpressionManager::areParenthesesBalanced(const std::string& expression) const {
+    // Implementation to check if parentheses in the expression are balanced
+}
+
+std::string ExpressionManager::convertInfixToPostfix(const std::string& infix) {
+    // Implementation to convert infix to postfix
+}
